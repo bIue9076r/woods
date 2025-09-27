9a05e450057de9c885368cd1e564504ccb422939
Maggie Shen <maggieshen@wireless-10-155-184-41.public.utexas.edu>
2025-09-27 00:25:29 -0500

    2

--- main.lua
+++ main.lua
@@ -2,20 +2,32 @@ require("globals")
 require("dialogue")
 
 function love.load()
-	
+	local f = Load_States[Gamestate] or Error_Update
+	f()
 end
 
 function love.update(dt)
-	local f = Update_States[Gamestate] or function (dt) end
-	if f then f(dt) end
+	local f = Update_States[Gamestate] or Error_Update
+	f(dt)
 end
 
 function love.keypressed(key)
-	local f = Keypressed_States[Gamestate] or function (key) end
-	if f then f(key) end
+	if key == 'space' then 
+		Gamestate = "Cutscene"
+	end
+	local f = Keypressed_States[Gamestate] or Error_Update
+	f(key)
 end
 
 function love.draw()
-	local f = Draw_States[Gamestate] or function () end
-	if f then f() end
+	local f = Draw_States[Gamestate] or Error_Update
+	f()
+end
+
+function love.mousepressed(x, y, button, istouch)
+   if button == 1 then
+		Gamestate = "Dialouge"
+		local f = Draw_States[Gamestate] or Error_Update
+		f()
+   end
 end
