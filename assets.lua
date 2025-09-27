50d69e9ed1287c79d1d1bf09b8fd45241867a060
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-09-26 23:31:22 -0500

    Globals

---
+++ assets.lua
@@ -0,0 +1,27 @@
+Sound = {Contents = {}}
+Image = {Contents = {}}
+Font = {Contents = {}}
+
+function Sound.new(Index, Path, Type)
+	Sound.Contents[Index] = love.audio.newSource(Path,Type or "stream")
+end
+
+function Sound.get(Index)
+	return Sound.Contents[Index] or Sound.Contents[0]
+end
+
+function Image.new(Index, Path)
+	Image.Contents[Index] = love.graphics.newImage(Path)
+end
+
+function Image.get(Index)
+	return Image.Contents[Index] or Image.Contents[0]
+end
+
+function Font.new(Index, Path, Size)
+	Font.Contents[Index] = love.graphics.newFont(Path, Size or 12)
+end
+
+function Font.get(Index)
+	return Font.Contents[Index] or Font.Contents[0]
+end
--- globals.lua
+++ globals.lua
@@ -1,17 +1,32 @@
-Gamestate = "Cutscene"
+require("assets")
+require("Assets/defines")
+Gamestate = "Intro"
 
 Load_States = {
+	["Intro"] = function() end,
 	["Cutscene"] = function() end,
+	["Dialouge"] = function() end, -- TODO: Use functions from dialouge.lua
 }
 
 Update_States = {
+	["Intro"] = function(dt) end,
 	["Cutscene"] = function (dt) end,
+	["Dialouge"] = function(dt) end, -- TODO: Use functions from dialouge.lua
 }
 
 Keypressed_States = {
+	["Intro"] = function(key) end,
 	["Cutscene"] = function (key) end,
+	["Dialouge"] = function(key) end, -- TODO: Use functions from dialouge.lua
 }
 
 Draw_States = {
+	["Intro"] = function() end,
 	["Cutscene"] = function () end,
+	["Dialouge"] = function() end, -- TODO: Use functions from dialouge.lua
 }
+
+
+function Error_Update(dt)
+	Gamestate = "Intro"
+end
