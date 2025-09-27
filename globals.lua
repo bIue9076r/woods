f75524b4437e990576e7d579869d343a85614adb
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-09-26 21:34:02 -0500

    Create globals.lua

---
+++ globals.lua
@@ -0,0 +1,17 @@
+Gamestate = ""
+
+Load_States = {
+	[""] = function() end,
+}
+
+Update_States = {
+	[""] = function (dt) end,
+}
+
+Keypressed_States = {
+	[""] = function (key) end,
+}
+
+Draw_States = {
+	[""] = function () end,
+}
