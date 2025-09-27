0931dcbef7c583225f77d38e6d8ed07cd89a1607
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-09-26 21:37:43 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,13 +5,16 @@ function love.load()
 end
 
 function love.update(dt)
-	
+	local f = Update_States[Gamestate] or function (dt) end
+	if f then f(dt) end
 end
 
 function love.keypressed(key)
-
+	local f = Keypressed_States[Gamestate] or function (key) end
+	if f then f(key) end
 end
 
 function love.draw()
-	love.graphics.print('Hello World!', 400, 300)
+	local f = Draw_States[Gamestate] or function () end
+	if f then f() end
 end
