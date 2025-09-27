fa10f4b7b11cb04f878ece8695655e9ac3fe5025
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-09-26 21:15:58 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+	
+end
+
+function love.update(dt)
+	
+end
+
+function love.keypressed(key)
+	
+end
+
+function love.draw()
+	love.graphics.print('Hello World!', 400, 300)
+end
