11b52176e1c2cca6296f4b9c2c94b5f68f6ef470
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-10-31 20:46:10 -0500

    Update Dialogue.lua

--- Story/Dialogue.lua
+++ Story/Dialogue.lua
@@ -1,8 +1,9 @@
 Likes_Chem = true
 Movie_Date = false
+Study_Together = false
 
 require("Story.Dialogue_Branch_Ends")
---require("Story.Dialogue_Branch_6")
+require("Story.Dialogue_Branch_6")
 require("Story.Dialogue_Branch_5")
 require("Story.Dialogue_Branch_4")
 require("Story.Dialogue_Branch_3")
