f7b3406817df8f97542880e52ee0a879904d4dd2
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-11-05 19:46:46 -0600

    Branches

--- Story/Dialogue_Branch_7.lua
+++ Story/Dialogue_Branch_7.lua
@@ -1,1 +1,19 @@
--- branch 7
+Red_7 = Tree.new({
+	
+},nil)
+
+Red_7_Alt = Tree.new({
+	
+},nil)
+
+Green_7 = Tree.new({
+	
+},nil)
+
+Green_7_Alt = Tree.new({
+	
+},nil)
+
+No_7 = Tree.new({
+	
+},nil)
---
+++ Story/Dialogue_Branch_8.lua
@@ -0,0 +1,51 @@
+Red_8 = Tree.new({
+	
+},nil)
+
+Red_8_Alt_1 = Tree.new({
+	
+},nil)
+
+Red_8_Alt_2 = Tree.new({
+	
+},nil)
+
+Red_8_Alt_3 = Tree.new({
+	
+},nil)
+
+Red_8_Alt_3_Branch_1 = Tree.new({
+	
+},nil)
+
+Red_8_Alt_3_Branch_2 = Tree.new({
+	
+},nil)
+
+Green_8 = Tree.new({
+	
+},nil)
+
+Green_8_Alt_1 = Tree.new({
+	
+},nil)
+
+Green_8_Alt_2_Alt_1 = Tree.new({
+	
+},nil)
+
+Green_8_Alt_2_Alt_2 = Tree.new({
+	
+},nil)
+
+Green_8_Alt_2_Alt_3 = Tree.new({
+	
+},nil)
+
+Green_8_Alt_2_Alt_4 = Tree.new({
+	
+},nil)
+
+No_8 = Tree.new({
+	
+},nil)
