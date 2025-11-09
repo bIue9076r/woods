a2b3d435f05710c955b7c1e0d59d5162b68c6f43
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-11-08 18:58:29 -0600

    Update Dialogue_Branch_8.lua

--- Story/Dialogue_Branch_8.lua
+++ Story/Dialogue_Branch_8.lua
@@ -1,73 +1,104 @@
 Red_8 = Tree.new({
 	Stem.new("Lorelai",1,"You're actually not really my type.",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Douglass",1,"..Are you serious?",nil),
+	Stem.new("Lorelai",1,"Yeah. Like very much so not my type.",nil),
+	Stem.new("Douglass",1,"Yeah uh.. thats alright.",nil),
+	Stem.new("Lorelai",1,"What do you mean?",nil),
+	Stem.new("Douglass",1,"I don’t think you’re uh.. my type either.",nil),
+	Stem.new("Lorelai",1,"Well, what the hell are we doing here then?",nil),
+	Stem.new("Douglass",1,"I thought you were really cool until you started-",nil),
+	Stem.new("Lorelai",1,"Oh please! You act like I punched your balls man.",nil),
+	Stem.new("Douglass",1,"You’re- you’re crazy, you know that?",nil),
+	Stem.new("Lorelai",1,"Noted.",nil),
 },nil)
 
 Red_8_Alt_1 = Tree.new({
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"You're actually not really my type.",nil),
+	Stem.new("Douglass",1,"..Are you serious?",nil),
+	Stem.new("Lorelai",1,"Yeah. Like very much so not my type.",nil),
+	Stem.new("Lorelai",1,"Really?",nil),
+	Stem.new("Lorelai",1,"Yeah.",nil),
+	Stem.new("Lorelai",1,"What, what about me is-",nil),
+	Stem.new("Lorelai",1,"Just not my type man.",nil),
+	Stem.new("Lorelai",1,"Yeah.. alright. sorry.",nil),
 },nil)
 
 Red_8_Alt_2 = Tree.new({
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"You're actually not really my type.",nil),
+	Stem.new("Douglass",1,"..Are you serious?",nil),
+	Stem.new("Lorelai",1,"Yeah. Like very much so not my type.",nil),
+	Stem.new("Douglass",1,"Wait.. you’re joking, right?",nil),
 },nil)
 
 Red_8_Alt_2_Branch_1 = Tree.new({
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"Yeah. that was a joke.",nil),
+	Stem.new("Lorelai",1,"Oh! Oh- yeah! yeah that.. that was funny. Really funny.",nil),
+	Stem.new("Lorelai",1,"You think so?",nil),
+	Stem.new("Lorelai",1,"Uh.. yeah.",nil),
 },nil)
 
 Red_8_Alt_2_Branch_2 = Tree.new({
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
-	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"No. Like I genuinely am not attracted to you.",nil),
+	Stem.new("Lorelai",1,"But i thought we- we had such a good connection!",nil),
+	Stem.new("Lorelai",1,"Well, yeah. But that doesn’t change the fact that i’m not interested.",nil),
+	Stem.new("Lorelai",1,"..Okay- ..Sorry.",nil),
 },nil)
 
 Green_8 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
 
 Green_8_Alt_1 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
 
 Green_8_Alt_2_Alt_1 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
 
 Green_8_Alt_2_Alt_2 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
 
 Green_8_Alt_2_Alt_3 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
 
 Green_8_Alt_2_Alt_4 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
 
 No_8 = Tree.new({
-	
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
+	Stem.new("Lorelai",1,"",nil),
 },nil)
